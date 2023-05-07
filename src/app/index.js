47800eadd3e08136420dcae63b2cf85c9dc8d5cf
Aldana-1985 <aldana25_85@hotmail.com>
2023-05-07 20:55:25 -0300

    Second commit

--- src/app/index.js
+++ src/app/index.js
@@ -1,4 +1,15 @@
 function laCajaDePandora(numero){
-    // proximamente escribiremos codigo aqui
+    if(numero % 2 === 0) {  
+        return numero.toString(2);  
+    } else {  
+        return numero.toString(16);  
+    }
 }
 
+function miInformacion() {
+    return {
+      nombre: "Aldana",
+      edad: 37,
+      nacionalidad: "Argentina"
+    };
+}
