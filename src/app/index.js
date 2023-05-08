1847eb128b322190587cabb59dfb1c5959454234
gonzalo <gonzasuarez100@gmail.com>
2023-05-07 21:29:14 -0300

    funcion de gonza

--- src/app/index.js
+++ src/app/index.js
@@ -1,3 +1,17 @@
-function laCajaDePandora(numero){
-    // proximamente escribiremos codigo aqui
+function laCajaDePandora(num){
+    if (num % 2 === 0) {
+        // El número es par
+        return num.toString(2); // Convierte a binario
+      } else {
+        // El número es impar
+        return num.toString(16); // Convierte a hexadecimal
+      }
     }
+
+function gonzaS() {
+    return {
+        nombre:'Gonza S',
+        edad: 26,
+        nacionalidad: 'Argentino'
+    }
+}
